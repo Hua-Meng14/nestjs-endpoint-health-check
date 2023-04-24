84a7ef2416cb7ca3afc6f018f3eded105d59cbb9
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-04-24 21:26:32 +0700

    add unit test case

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
 import { mockDeep, MockProxy } from 'jest-mock-extended';
+import { DiskHealthIndicator, HealthCheckService, MemoryHealthIndicator, TypeOrmHealthIndicator, HealthCheckStatus } from '@nestjs/terminus';
+
 describe('HealthController', () => {
   let controller: HealthController;
+  let healthCheckService: MockProxy<HealthCheckService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: mockDeep<HealthCheckService>(),
+        },
+        {
+          provide: TypeOrmHealthIndicator,
+          useValue: mockDeep<TypeOrmHealthIndicator>(),
+        },
+        {
+          provide: MemoryHealthIndicator,
+          useValue: mockDeep<MemoryHealthIndicator>(),
+        },
+        {
+          provide: DiskHealthIndicator,
+          useValue: mockDeep<DiskHealthIndicator>(),
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
+    healthCheckService = module.get(HealthCheckService);
   });
+  describe('check', () => {
+    it('should return the health of various components', async () => {
+      const mockResult = {
+        database: { status: 'up' },
+        memory_heap: { status: 'up' },
+        memory_rss: { status: 'up' },
+        storage: { status: 'up' },
+        details: {},
+        status: 'ok' as HealthCheckStatus,
+      }
+      healthCheckService.check.mockImplementation(async () => mockResult);
+      const result = await controller.check();
+      console.log(result)
+      expect(result).toEqual(mockResult)
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  })
 });
